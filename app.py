4f479262c5eca3275a9a014e01b7d43d1149ee7a
KaustubhDamania <kaustubh.damania@gmail.com>
2019-07-04 14:19:03 +0530

    Added

--- app.py
+++ app.py
@@ -1,16 +1,47 @@
 from flask import Flask,request,render_template
 from pprint import pprint
 import json
+import os
+import dialogflow
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
+
 app = Flask(__name__)
+app.config['SECRET_KEY']='5791628bb0b13ce0c676dfde280ba245'
+
+class MessageBox(FlaskForm):
+    message = StringField('Message', validators=[DataRequired()])
+    submit = SubmitField('Send')
 
 @app.route('/',methods=['GET','POST'])
+def get_response():
+    form = MessageBox()
+    text_input, query_input, response,fulfillment_msg, fulfillment_text = [None]*5
+    if form.validate_on_submit():
+        message = form.message.data
+        print('message',message)
+        language_code = 'en'
+        project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
+        session_client = dialogflow.SessionsClient()
+        session = session_client.session_path(project_id, "unique")
+        print(session)
+        if message:
+            text_input = dialogflow.types.TextInput(text=message,
+                                                    language_code=language_code)
+            query_input = dialogflow.types.QueryInput(text=text_input)
+            response = session_client.detect_intent(session=session,
+                                                    query_input=query_input)
 
-def post():
-    req = request.get_json(silent=True, force=True)
-    pprint(req)
-    myvar=""
-    return render_template('tp.html')# json.dumps({'fulfillmentText':'Welcome to Chatbot nibba!'})
-
+        if response:
+            fulfillment_msg = response.query_result.fulfillment_text
+            fulfillment_arr = response.query_result.fulfillment_messages
+            if str(fulfillment_arr[0].text.text[0]) != '':
+                fulfillment_text = fulfillment_arr[0].text.text[0]
+            else:
+                fulfillment_text = fulfillment_msg
+    print(fulfillment_msg)
+    return render_template('tp.html',form=form,a=fulfillment_msg)
 
 if __name__=='__main__':
-    app.run()
+    app.run(debug=False)
--- test.py
+++ test.py
@@ -1,7 +1,32 @@
 from flask import Flask,request
-import firebase_admin
-
+import os
 app = Flask(__name__)
+
+def detect_intent_texts(project_id, session_id, text, language_code):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    if text:
+        text_input = dialogflow.types.TextInput(
+            text=text, language_code=language_code)
+        query_input = dialogflow.types.QueryInput(text=text_input)
+        response = session_client.detect_intent(
+            session=session, query_input=query_input)
+        # print(response.query_result, str(response.query_result.fulfillment_messages[0].text)=='', response.query_result.fulfillment_messages[0].text=='')
+        if str(response.query_result.fulfillment_messages[0].text)!='':
+            return str(response.query_result.fulfillment_messages[0].text.text[0]),response.query_result.fulfillment_text
+        else:
+            return response.query_result.fulfillment_text, response.query_result.fulfillment_text
+
+
+@app.route('/',methods=['GET','POST'])
+def dialogf():
+    import dialogflow
+    project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
+    message = 'hey'
+    fulfillment_text, fulfillment_msg = detect_intent_texts(project_id, "unique", message, 'en')
+    #print('msg',fulfillment_msg,'txt',fulfillment_text)
+    return "Hello World!"
 
 @app.route('/tp',methods=['GET','POST'])
 def get():
@@ -11,5 +36,37 @@ def get():
     print(req)
     return 'This works!'
 
+@app.route('/dbcheck',methods=['GET','POST'])
+def dbcheck():
+    import firebase_admin
+    from firebase_admin import credentials
+    path_json = os.getenv('FIREBASE_CREDENTIALS')
+    cred = credentials.Certificate(path_json)
+    db_app = firebase_admin.initialize_app(cred, {
+        'databaseURL' : 'https://cabot-xuhseu.firebaseio.com'
+    })
+    # db_app = firebase_admin.initialize_app()
+
+    from firebase_admin import db
+
+    obj = db.reference('aadhar_data')
+    try:
+        print(obj, len(obj))
+    except Exception as e:
+        print('Doesnt work that way')
+
+    def search(name):
+        for i in range(1000):
+            obj = db.reference('aadhar_data/{}'.format(i))
+            if obj.get().get('name')==name:
+                print(obj.get().get('aadhar_number'))
+                break
+            print(i)
+        # print(obj.get())
+    search("Namasri Bhanushali") #107th entry
+    return 'Hellow'
+
+
+
 if __name__=='__main__':
-    app.run()
+    app.run(debug=False)
