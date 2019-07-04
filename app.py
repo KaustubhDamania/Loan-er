07e9bc3c77e422c28241e6c82159ff5289865afa
KaustubhDamania <kaustubh.damania@gmail.com>
2019-07-04 22:36:45 +0530

    Refactored stuff

--- app.py
+++ app.py
@@ -14,32 +14,39 @@ class MessageBox(FlaskForm):
     message = StringField('Message', validators=[DataRequired()])
     submit = SubmitField('Send')
 
+def get_fulfillment_texts(message, language_code, project_id):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, "unique")
+    print(session)
+    if message:
+        text_input = dialogflow.types.TextInput(text=message,
+                                                language_code=language_code)
+        query_input = dialogflow.types.QueryInput(text=text_input)
+        response = session_client.detect_intent(session=session,
+                                                query_input=query_input)
+
+    if response:
+        fulfillment_msg = response.query_result.fulfillment_text
+        fulfillment_arr = response.query_result.fulfillment_messages
+        if str(fulfillment_arr[0].text.text[0]) != '':
+            fulfillment_text = fulfillment_arr[0].text.text[0]
+        else:
+            fulfillment_text = fulfillment_msg
+
+    return fulfillment_msg, fulfillment_text
+
+
 @app.route('/',methods=['GET','POST'])
 def get_response():
     form = MessageBox()
     text_input, query_input, response,fulfillment_msg, fulfillment_text = [None]*5
     if form.validate_on_submit():
         message = form.message.data
-        print('message',message)
+        # print('message',message)
         language_code = 'en'
         project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
-        session_client = dialogflow.SessionsClient()
-        session = session_client.session_path(project_id, "unique")
-        print(session)
-        if message:
-            text_input = dialogflow.types.TextInput(text=message,
-                                                    language_code=language_code)
-            query_input = dialogflow.types.QueryInput(text=text_input)
-            response = session_client.detect_intent(session=session,
-                                                    query_input=query_input)
-
-        if response:
-            fulfillment_msg = response.query_result.fulfillment_text
-            fulfillment_arr = response.query_result.fulfillment_messages
-            if str(fulfillment_arr[0].text.text[0]) != '':
-                fulfillment_text = fulfillment_arr[0].text.text[0]
-            else:
-                fulfillment_text = fulfillment_msg
+        fulfillment_msg, fulfillment_text = get_fulfillment_texts(message,
+                                                        language_code, project_id)
     print(fulfillment_msg)
     return render_template('tp.html',form=form,a=fulfillment_msg)
 
