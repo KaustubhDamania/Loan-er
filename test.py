89b0303e3953731e9939eb81281c5aa1e225266b
KaustubhDamania <kaustubh.damania@gmail.com>
2019-07-07 02:42:53 +0530

    Added firestore tests

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from flask import Flask,request
 import os
+import firebase_admin
+from firebase_admin import credentials
 app = Flask(__name__)
 
 def detect_intent_texts(project_id, session_id, text, language_code):
@@ -18,8 +20,8 @@ def detect_intent_texts(project_id, session_id, text, language_code):
         else:
             return response.query_result.fulfillment_text, response.query_result.fulfillment_text
 
-
-@app.route('/',methods=['GET','POST'])
+# Route for dialogflow api testing
+@app.route('/dialogf',methods=['GET','POST'])
 def dialogf():
     import dialogflow
     project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
@@ -28,6 +30,7 @@ def dialogf():
     #print('msg',fulfillment_msg,'txt',fulfillment_text)
     return "Hello World!"
 
+# Route for trying request library
 @app.route('/tp',methods=['GET','POST'])
 def get():
     req = request.get_data()
@@ -36,10 +39,9 @@ def get():
     print(req)
     return 'This works!'
 
+# Route for firebase realtime database
 @app.route('/dbcheck',methods=['GET','POST'])
 def dbcheck():
-    import firebase_admin
-    from firebase_admin import credentials
     path_json = os.getenv('FIREBASE_CREDENTIALS')
     cred = credentials.Certificate(path_json)
     db_app = firebase_admin.initialize_app(cred, {
@@ -66,6 +68,7 @@ def dbcheck():
     search("Namasri Bhanushali") #107th entry
     return 'Hellow'
 
+# Route for language detection
 @app.route('/lang',methods=['GET','POST'])
 def lang():
     from langdetect import detect_langs
@@ -73,6 +76,35 @@ def lang():
     languages = detect_langs(message)
     languages = [language.lang for language in languages]
     return message+' '+str(languages)
+
+# Route for firebase firestore
+@app.route('/store',methods=['GET','POST'])
+def store():
+    from google.cloud import firestore
+    fire = firestore.Client()
+
+    #Push some data
+    aadhar_ref = fire.collection(u'aadhar_data')
+
+    # Get some data
+    print(aadhar_ref.document(u'0').get().to_dict())
+
+    # Query data
+    from time import time
+    init_time = time()
+    query_result1 = aadhar_ref.where('aadhar_number',u'==',u'033878102619').get()
+    time_taken = time()-init_time
+    print('time_taken #1', time_taken)
+    for documents in query_result1:
+        print(documents.to_dict(), documents.id)
+
+    query_result2 = aadhar_ref.where('aadhar_number',u'>',u'0').get() #Worst Case
+    time_taken = time()-init_time
+    print('time_taken #2', time_taken)
+
+    # for documents in query_result2: #large output
+    #     print(documents.to_dict(), documents.id)
+    return 'Some'
 
 
 if __name__=='__main__':
