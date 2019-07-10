0c4e9c7e451ead12317488d62a3cc2f16d18a86c
KaustubhDamania <kaustubh.damania@gmail.com>
2019-07-10 18:43:21 +0530

    Added bank data + multiple message fix

--- app.py
+++ app.py
@@ -3,10 +3,8 @@ from pprint import pprint
 import json
 import os
 import dialogflow
+from secrets import token_hex
 from google.cloud import firestore
-from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
-from wtforms.validators import DataRequired
 from langdetect import detect_langs
 from googletrans import Translator
 from emoji import emojize, demojize
@@ -17,11 +15,10 @@ SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 translator = Translator()
 db = firestore.Client()
-
-
-class MessageBox(FlaskForm):
-    message = StringField('Message', validators=[DataRequired()])
-    submit = SubmitField('Send')
+document_name = token_hex(16)
+fields = ['aadhar','aadhar_pic1','aadhar_pic2','bank_acc','email','ifsc','loan_amt',
+        'loan_duration','name','pan','pan_photo','phone_no']
+user_data = {i:None for i in fields}
 
 def get_fulfillment_texts(message, project_id):
     session_client = dialogflow.SessionsClient()
@@ -32,32 +29,39 @@ def get_fulfillment_texts(message, project_id):
         query_input = dialogflow.types.QueryInput(text=text_input)
         response = session_client.detect_intent(session=session,
                                                 query_input=query_input)
-        print('RESPONSE')
-        pprint(response)
+        # print('RESPONSE')
+        # pprint(response)
 
     if response:
         fulfillment_msg = response.query_result.fulfillment_text
         fulfillment_arr = response.query_result.fulfillment_messages
+        new_arr=[]
+        for item in fulfillment_arr:
+            # print('fulfillment_messages',item)
+            new_arr.append({
+                'text': {
+                    'text': [item.text.text[0]]
+                }
+            })
+        print(new_arr)
         intent_name = response.query_result.intent
-        intent_name = {
+        intent = {
             "name": intent_name.name,
             "displayName": intent_name.display_name,
             "isFallback": (intent_name.name=='Default Fallback Intent')
         }
-        if str(fulfillment_arr[0].text.text[0]) != '':
-            fulfillment_text = fulfillment_arr[0].text.text[0]
-        else:
-            fulfillment_text = fulfillment_msg
+        # if str(fulfillment_arr[0].text.text[0]) != '':
+        #     fulfillment_text = fulfillment_arr[0].text.text[0]
+        # else:
+        #     fulfillment_text = fulfillment_msg
 
-    return fulfillment_msg, fulfillment_text, intent_name
+    return fulfillment_msg, new_arr, intent
 
 def convert_to_hi(fulfillment_msg):
     fulfillment_msg = demojize(fulfillment_msg)
     fulfillment_msg = translator.translate(fulfillment_msg, src='en', dest='hi').text
     pattern = re.compile(r':(.*?):')
     emoji_indices = [m.span() for m in re.finditer(pattern,fulfillment_msg)]
-    # print('fulfillment_msg',fulfillment_msg)
-    # print('emoji_indices', emoji_indices)
 
     # for i,j in emoji_indices:
     while len(emoji_indices)>0:
@@ -86,10 +90,6 @@ def get_language_code(message):
         pass
     return language_code
 
-def get_message(user_message):
-    return user_message
-
-
 check=False
 @app.route('/myapi', methods=['POST'])
 def myapi():
@@ -103,15 +103,11 @@ def myapi():
         message = bytes.decode(req)
         try:
             message = jsonify(message)
-            # print('dict',message)
-            # print(type(message),dir(message))
-            # print(message.json)
             message = json.loads(message.json)['queryResult']['queryText']
         except Exception as e:
             print('Exception: {}'.format(e))
             message = bytes.decode(req)
             pass
-        print('message',message)
         project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
         language_code = get_language_code(message)
 
@@ -121,9 +117,11 @@ def myapi():
         # print('language',language_code,'translated_text',message)
 
         fulfillment_text, fulfillment_msg, intent = get_fulfillment_texts(message, project_id)
-
+        intent_name = intent['displayName']
+        print('intent_name',intent_name)
         if language_code == 'hi':
-            fulfillment_msg = convert_to_hi(fulfillment_msg)
+            for i in range(len(fulfillment_msg)):
+                fulfillment_msg[i]['text']['text'][0] = convert_to_hi(fulfillment_msg[i]['text']['text'][0])
             fulfillment_text = convert_to_hi(fulfillment_text)
 
         # response = {'reply':fulfillment_msg}
@@ -131,48 +129,13 @@ def myapi():
         response = {
             "intent": intent,
             "fulfillmentText": fulfillment_text,
-            "fulfillmentMessages": [
-                {
-                    "text": {
-                        "text":[fulfillment_msg]
-                    }
-                }]
+            "fulfillmentMessages": fulfillment_msg
         }
         check=False
         return jsonify(response)
 
     return jsonify({'reply':'404'})
 
-@app.route('/',methods=['GET','POST'])
-def get_response():
-    form = MessageBox()
-    # text_input, query_input, response,fulfillment_msg, fulfillment_text = [None]*5
-    # language_code = 'en'
-    fulfillment_msg = None
-    if form.validate_on_submit():
-        import requests
-        message = form.message.data
-        print('url',request.url+'myapi')
-        print('message',message)
-        response = requests.post(url=request.url+'myapi',data=message.encode('utf-8'))
-        response = json.loads(response.text)
-        fulfillment_msg = response.get('fulfillmentText')
-    #     message = get_message(form.message.data)
-    #     set_language_code()
-    #
-    #     if language_code == 'hi':
-    #         message = translator.translate(message, src='hi', dest='en').text
-    #
-    #     project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
-    #     fulfillment_msg, fulfillment_text = get_fulfillment_texts(message, project_id)
-    #     print('message',message)
-    #
-    # if language_code == 'hi':
-    #     fulfillment_msg = convert_to_hi(fulfillment_msg)
-
-    # print(fulfillment_msg)
-    return render_template('tp.html',form=form,a=fulfillment_msg)
-
 
 if __name__=='__main__':
-    app.run(debug=False)
+    app.run()
--- data/Data Gen Scripts/bank_details.py
+++ data/Data Gen Scripts/bank_details.py
@@ -3,8 +3,16 @@ from pprint import pprint
 import json
 from random import randint, seed
 from selenium import webdriver
-from time import time
+from time import time,sleep
 branch_codes = {}
+# with open('bank_data.json') as f:
+#     temp = json.load(f)
+# temp=temp['bank_data']
+# for item in temp:
+#     branch = item['bank_branch']
+#     branch_code = item['bank_acc_no'][:4]
+#     branch_codes[branch]=branch_code
+
 list_to_str = lambda arr: ''.join(arr)
 bank_data = []
 from selenium.webdriver.firefox.options import Options
@@ -19,7 +27,7 @@ def bank_accno_gen(branch):
     else:
         branch_code = [str(randint(0,9)) for i in range(4)]
         branch_code = list_to_str(branch_code)
-        branch_codes['branch'] = branch_code
+        branch_codes[branch] = branch_code
     return branch_code + list_to_str([str(randint(0,9)) for i in range(11)])
 
 def list_to_json(arr,file_name):
@@ -30,7 +38,7 @@ def list_to_json(arr,file_name):
         json.dump(temp, fp, indent=4)
 
 
-user = 0
+user = 500
 with open('../pan_data.json') as f:
     pan_data = json.load(f)
 pan_data = pan_data['pan_data']
@@ -45,9 +53,12 @@ while user<1000:
         arr = []
         for i in range(4):
             lol=elems[i].find_elements_by_tag_name('option')
+            # print(i,[a.text for a in lol])
             elem = lol[randint(1,len(lol)-1)]
             arr.append(elem.text)
+            # print(arr[-1])
             elem.click()
+            sleep(0.25)
         arr.append(browser.find_element_by_class_name('resultElement').find_element_by_tag_name('p').text)
         branch = arr[-2]
         ifsc = arr[-1]
@@ -62,12 +73,12 @@ while user<1000:
         }
         bank_data.append(temp)
         user += 1
-        if user%250==0:
-            list_to_json(bank_data, 'bank_data.json')
+        if user%10==0:
+            list_to_json(bank_data, 'bank_data1.json')
         print('Iteration no {}, time taken is {}'.format(user,time()-init_time))
     # print(bank_data)
     except Exception as e:
         print(e)
     finally:
         browser.close()
-list_to_json(bank_data,'bank_data.json')
+list_to_json(bank_data,'bank_data1.json')
