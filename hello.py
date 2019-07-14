225cdc690a0adf56115128d78e956b82c01dbf9f
KaustubhDamania <kaustubh.damania@gmail.com>
2019-07-14 19:08:56 +0530

    Optimized function calls

--- hello.py
+++ hello.py
@@ -107,6 +107,7 @@ fields = ['aadhar_no','aadhar_pic1','aadhar_pic2','bank_acc','email','ifsc','loa
         'loan_duration','name','pan','pan_photo','phone_no']
 user_data = {i:None for i in fields}
 isHindi = False
+filename = None
 
 def pan_check(pan):
     if len(pan)!=10:
@@ -221,242 +222,7 @@ def replace_text(pattern, replacement, fulfillment_msg):
     latter_part = fulfillment_msg[0]['text']['text'][0][indices[1]:]
     fulfillment_msg[0]['text']['text'][0] = first_part+str(replacement)+latter_part
 
-check=False
-@app.route('/myapi', methods=['POST'])
-def myapi():
-    global check
-    import requests
-    key,filename,message='','',''
-    count=1
-
-    #This jugaad ensures that the POST request sent by Dialogflow is not processed
-    if not check:
-        check=True
-        req = request.get_data() #message received through user's post request
-        # req = request.get_json(force=True)
-        print('data',req)
-        print(request.files)
-        # print(request.headers.get('Content-Type'))
-        # print('image' not in request.headers.get('Content-Type'))
-        if not request.files:
-            message = bytes.decode(req)
-        else:
-            keys = request.files.keys()
-            for i in keys:
-                key=i
-            file = request.files[key]
-            # print(dir(file))
-            # TODO: Upload this image to storage
-            filename = '{}_{}.jpg'.format(token_hex(8),count)
-            file.save(os.path.join(os.getcwd(),filename))
-            count += 1
-            message = file.filename
-        try:
-            message = jsonify(message)
-            message = json.loads(message.json)['queryResult']['queryText']
-        except Exception as e:
-            print('Exception: {}'.format(e))
-            if not request.files:
-                message = bytes.decode(req)
-            else:
-                message = request.files['media'].filename
-
-        project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
-        language_code = get_language_code(message)
-        filename = '{}_{}.jpg'.format(token_hex(8),count)
-        if language_code == 'hi':
-            message = translator.translate(message, src='hi', dest='en').text
-
-        # print('language',language_code,'translated_text',message)
-
-        fulfillment_text, fulfillment_msg, response = get_fulfillment_texts(message, project_id)
-        intent_name = response.query_result.intent
-        intent = {
-            "name": intent_name.name,
-            "displayName": intent_name.display_name,
-            "isFallback": (intent_name.display_name=='Default Fallback Intent')
-        }
-        # intent = {
-        #     "name": "projects/cabot-xuhseu/agent/intents/fe84a4dd-e03e-4ebf-a9f2-e872bcfa9282",
-        #     "displayName": "Default Fallback Intent",
-        #     "isFallback": True
-        # }
-        intent_name = intent['displayName']
-        print('intent_name',intent_name)
-        print('isHindi',isHindi)
-
-        if intent_name=='loan':
-            # pprint(dir(response))
-            pass
-        elif intent_name=='get name':
-            print('name is',response.query_result.output_contexts[-1].parameters['name'])
-            user_data['name']=response.query_result.output_contexts[-1].parameters['name']
-
-        elif intent_name=='amount-1':
-            print('amount is',int(response.query_result.output_contexts[-1].parameters['amount']))
-            user_data['loan_amt']=int(response.query_result.output_contexts[-1].parameters['amount'])
-
-        elif intent_name=='loan period':
-            print('duration is',int(response.query_result.output_contexts[-1].parameters['duration']['amount']))
-            user_data['loan_duration']=int(response.query_result.output_contexts[-1].parameters['duration']['amount'])
-
-        elif intent_name=='email':
-            print('email is',(response.query_result.output_contexts[-1].parameters['email']))
-            user_data['email']=response.query_result.output_contexts[-1].parameters['email']
-
-        elif intent_name=='pan':
-            user_text = response.query_result.query_text
-            pan = response.query_result.output_contexts[-1].parameters['pan']
-            if pan=='':
-                for word in user_text.split():
-                    if pan_check(word):
-                        pan = word
-                        break
-            print('pan is',pan)
-            user_data['pan'] = pan
-
-        elif intent_name=='PAN pic upload':
-            # upload_pic(filename)
-            user_data['pan_photo'] = filename
-            count += 1
-            # os.remove(filename)
-
-        elif intent_name=='Aadhar number':
-            print('aadhar is',str(int(response.query_result.output_contexts[-1].parameters['aadhar'])))
-            user_data['aadhar_no'] = str(int(response.query_result.output_contexts[-1].parameters['aadhar']))
-
-        elif intent_name=='Aadhar pic front':
-            # upload_pic(filename)
-            user_data['aadhar_pic1'] = filename
-            count += 1
-            # os.remove(filename)
-
-        elif intent_name=='Aadhar pic back':
-            # upload_pic(filename)
-            user_data['aadhar_pic2'] = filename
-            # os.remove(filename)
-            # credit_score = db.collection(u'credit_score_data').document(document_name).get().to_dict().get('credit_score')
-            credit_ref = db.collection(u'credit_score_data')
-            credit_score = randint(0,900)
-            try:
-                query_result1 = credit_ref.where('pan',u'==',user_data['pan']).get()
-                for i in query_result1:
-                    credit_score = i.to_dict()['credit_score']
-            except Exception as e:
-                print('This is an Exception')
-                print(e)
-                print('End exception')
-
-            if credit_score < 500:
-                loaner = 0
-            else:
-                loaner = ((credit_score-500)/400)*int(user_data['loan_amt'])
-
-            print('LOLOLOL',fulfillment_msg)
-            replace_text(r'XXXX',loaner, fulfillment_msg)
-            replace_text(r'YY',user_data['loan_duration'], fulfillment_msg)
-            replace_text(r'ZZZZ',calc_emi(user_data['loan_amt'],user_data['loan_duration']), fulfillment_msg)
-            # pattern = re.compile(r'XXXX')
-            # indices = [m.span() for m in re.finditer(pattern,fulfillment_msg[0]['text']['text'][0])]
-            # indices = indices[0]
-            # first_part = fulfillment_msg[0]['text']['text'][0][:indices[0]]
-            # latter_part = fulfillment_msg[0]['text']['text'][0][indices[1]:]
-            # fulfillment_msg[0]['text']['text'][0] = first_part+str(loaner)+latter_part
-            #
-            # pattern = re.compile(r'YY')
-            # indices = [m.span() for m in re.finditer(pattern,fulfillment_msg[0]['text']['text'][0])]
-            # indices = indices[0]
-            # first_part = fulfillment_msg[0]['text']['text'][0][:indices[0]]
-            # latter_part = fulfillment_msg[0]['text']['text'][0][indices[1]:]
-            # fulfillment_msg[0]['text']['text'][0] = first_part+str(user_data['loan_duration'])+latter_part
-            #
-            # pattern = re.compile(r'ZZZZ')
-            # indices = [m.span() for m in re.finditer(pattern,fulfillment_msg[0]['text']['text'][0])]
-            # indices = indices[0]
-            # first_part = fulfillment_msg[0]['text']['text'][0][:indices[0]]
-            # latter_part = fulfillment_msg[0]['text']['text'][0][indices[1]:]
-            # fulfillment_msg[0]['text']['text'][0] = first_part+str(calc_emi(user_data['loan_amt'],user_data['loan_duration']))+latter_part
-
-        elif intent_name=='Loan approved - yes':
-            pass
-
-        elif intent_name=='Loan approved - no':
-            pass
-
-        elif intent_name=='Bank details':
-            user_text = response.query_result.query_text
-            user_text = user_text.split('\n')
-            user_data['bank_acc']=user_text[0]
-            user_data['ifsc']=user_text[1]
-            EMAIL_ADDRESS = "codeadventurebot@gmail.com"
-            EMAIL_PASSWORD = "17110667071"
-            with smtplib.SMTP('smtp.gmail.com',587) as smtp:
-                smtp.ehlo()
-                smtp.starttls()
-                smtp.ehlo()
-                smtp.login(EMAIL_ADDRESS,EMAIL_PASSWORD)
-                subject="Congratulations! You have completed the loan process"
-                body="blah blah"
-                msg=f'Subject:{subject}\n\n{body}'
-                smtp.sendmail(EMAIL_ADDRESS,user_data['email'],msg)
-
-        pprint(user_data)
-        if intent_name != 'Default Fallback Intent':
-            db.collection('user_data').document(document_name).set(user_data)
-
-        if language_code == 'hi' or isHindi:
-            for i in range(len(fulfillment_msg)):
-                fulfillment_msg[i]['text']['text'][0] = convert_to_hi(fulfillment_msg[i]['text']['text'][0])
-            fulfillment_text = convert_to_hi(fulfillment_text)
-
-        # response = {'reply':fulfillment_msg}
-        # fulfillment_text, fulfillment_msg = ['Welcome to Hell!']*2
-        response = {
-            "intent": intent,
-            "fulfillmentText": fulfillment_text,
-            "fulfillmentMessages": fulfillment_msg
-        }
-        check=False
-        return jsonify(response)
-
-    return jsonify({'reply':'404'})
-
-@app.route("/sms",methods=['POST'])
-def sms_reply():
-    #re
-    msg=request.form.get('Body')
-    phoneno=request.form.get('From')
-    #mediaurlx=request.form.get('MediaUrlX')
-    #print("mediaurlx",mediaurlx)
-    num_media = int(request.values.get("NumMedia"))
-    for idx in range(num_media):
-        media_url = request.values.get(f'MediaUrl{idx}')
-        print("urls",media_url)
-    print("msg",msg)
-    print("phoneno",phoneno)
-    if(num_media):
-        msg="img.png"    #create reply
-    # reply=fetch_reply(msg,phoneno)
-    reply,arr,response = get_fulfillment_texts(msg, os.getenv('DIALOGFLOW_PROJECT_ID'))
-    intent_name = response.query_result.intent
-    intent = {
-        "name": intent_name.name,
-        "displayName": intent_name.display_name,
-        "isFallback": (intent_name.display_name=='Default Fallback Intent')
-    }
-    fulfillment_msg = arr.copy()
-    fulfillment_text = reply
-    # intent = {
-
-    #     "name": "projects/cabot-xuhseu/agent/intents/fe84a4dd-e03e-4ebf-a9f2-e872bcfa9282",
-    #     "displayName": "Default Fallback Intent",
-    #     "isFallback": True
-    # }
-    intent_name = intent['displayName']
-    filename = '{}.jpg'.format(token_hex(8))
-    print('intent_name',intent_name)
-    print('isHindi',isHindi)
-
+def get_user_data(response,intent_name, fulfillment_msg):
     if intent_name=='loan':
         # pprint(dir(response))
         pass
@@ -549,6 +315,130 @@ def sms_reply():
             smtp.sendmail(EMAIL_ADDRESS,user_data['email'],msg)
 
     pprint(user_data)
+    return user_data
+
+check=False
+@app.route('/myapi', methods=['POST'])
+def myapi():
+    global check
+    import requests
+    key,filename,message='','',''
+    count=1
+
+    #This jugaad ensures that the POST request sent by Dialogflow is not processed
+    if not check:
+        check=True
+        req = request.get_data() #message received through user's post request
+        # req = request.get_json(force=True)
+        print('data',req)
+        print(request.files)
+        # print(request.headers.get('Content-Type'))
+        # print('image' not in request.headers.get('Content-Type'))
+        if not request.files:
+            message = bytes.decode(req)
+        else:
+            keys = request.files.keys()
+            for i in keys:
+                key=i
+            file = request.files[key]
+            # print(dir(file))
+            # TODO: Upload this image to storage
+            filename = '{}_{}.jpg'.format(token_hex(8),count)
+            file.save(os.path.join(os.getcwd(),filename))
+            count += 1
+            message = file.filename
+        try:
+            message = jsonify(message)
+            message = json.loads(message.json)['queryResult']['queryText']
+        except Exception as e:
+            print('Exception: {}'.format(e))
+            if not request.files:
+                message = bytes.decode(req)
+            else:
+                message = request.files['media'].filename
+
+        project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
+        language_code = get_language_code(message)
+        filename = '{}_{}.jpg'.format(token_hex(8),count)
+        if language_code == 'hi':
+            message = translator.translate(message, src='hi', dest='en').text
+
+        # print('language',language_code,'translated_text',message)
+
+        fulfillment_text, fulfillment_msg, response = get_fulfillment_texts(message, project_id)
+        intent_name = response.query_result.intent
+        intent = {
+            "name": intent_name.name,
+            "displayName": intent_name.display_name,
+            "isFallback": (intent_name.display_name=='Default Fallback Intent')
+        }
+        # intent = {
+        #     "name": "projects/cabot-xuhseu/agent/intents/fe84a4dd-e03e-4ebf-a9f2-e872bcfa9282",
+        #     "displayName": "Default Fallback Intent",
+        #     "isFallback": True
+        # }
+        intent_name = intent['displayName']
+        print('intent_name',intent_name)
+        print('isHindi',isHindi)
+
+        user_data = get_user_data(response,intent_name,fulfillment_msg)
+        if intent_name != 'Default Fallback Intent':
+            db.collection('user_data').document(document_name).set(user_data)
+
+        if language_code == 'hi' or isHindi:
+            for i in range(len(fulfillment_msg)):
+                fulfillment_msg[i]['text']['text'][0] = convert_to_hi(fulfillment_msg[i]['text']['text'][0])
+            fulfillment_text = convert_to_hi(fulfillment_text)
+
+        # response = {'reply':fulfillment_msg}
+        # fulfillment_text, fulfillment_msg = ['Welcome to Hell!']*2
+        response = {
+            "intent": intent,
+            "fulfillmentText": fulfillment_text,
+            "fulfillmentMessages": fulfillment_msg
+        }
+        check=False
+        return jsonify(response)
+
+    return jsonify({'reply':'404'})
+
+@app.route("/sms",methods=['POST'])
+def sms_reply():
+    #re
+    msg=request.form.get('Body')
+    phoneno=request.form.get('From')
+    #mediaurlx=request.form.get('MediaUrlX')
+    #print("mediaurlx",mediaurlx)
+    num_media = int(request.values.get("NumMedia"))
+    for idx in range(num_media):
+        media_url = request.values.get(f'MediaUrl{idx}')
+        print("urls",media_url)
+    print("msg",msg)
+    print("phoneno",phoneno)
+    if(num_media):
+        msg="img.png"    #create reply
+    # reply=fetch_reply(msg,phoneno)
+    reply,arr,response = get_fulfillment_texts(msg, os.getenv('DIALOGFLOW_PROJECT_ID'))
+    intent_name = response.query_result.intent
+    intent = {
+        "name": intent_name.name,
+        "displayName": intent_name.display_name,
+        "isFallback": (intent_name.display_name=='Default Fallback Intent')
+    }
+    fulfillment_msg = arr.copy()
+    fulfillment_text = reply
+    # intent = {
+
+    #     "name": "projects/cabot-xuhseu/agent/intents/fe84a4dd-e03e-4ebf-a9f2-e872bcfa9282",
+    #     "displayName": "Default Fallback Intent",
+    #     "isFallback": True
+    # }
+    intent_name = intent['displayName']
+    filename = '{}.jpg'.format(token_hex(8))
+    print('intent_name',intent_name)
+    print('isHindi',isHindi)
+
+    user_data = get_user_data(response,intent_name,fulfillment_msg)
     if intent_name != 'Default Fallback Intent':
         db.collection('user_data').document(document_name).set(user_data)
     resp=MessagingResponse()
